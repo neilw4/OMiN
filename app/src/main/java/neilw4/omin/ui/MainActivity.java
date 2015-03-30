3b312ca52449dd14f7636d1c0f409d62728dc949
Neil Wells <neilw4@googlemail.com>
2015-03-30 18:00:01 +0100

    show/change username in UI

--- app/src/main/java/neilw4/omin/ui/MainActivity.java
+++ app/src/main/java/neilw4/omin/ui/MainActivity.java
@@ -2,35 +2,18 @@ package neilw4.omin.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import neilw4.omin.Logger;
 import neilw4.omin.R;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     ViewPager mViewPager;
+    UnameManager mUidManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,17 +26,11 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
@@ -61,41 +38,20 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
             }
         });
 
-        // For each of the sections in the app, add a tab to the action bar.
         for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
             actionBar.addTab(
                     actionBar.newTab()
                             .setText(mSectionsPagerAdapter.getPageTitle(i))
                             .setTabListener(this));
         }
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        return super.onOptionsItemSelected(item);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        actionBar.setCustomView(R.layout.actionbar_main);
+        mUidManager = new UnameManager(this, actionBar.getCustomView());
     }
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
         mViewPager.setCurrentItem(tab.getPosition());
     }
 
@@ -105,41 +61,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 2 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Messages";
-                case 1:
-                    return "Following";
-            }
-            return null;
-        }
     }
 
 }
---
+++ app/src/main/java/neilw4/omin/ui/UnameManager.java
@@ -0,0 +1,195 @@
+package neilw4.omin.ui;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.text.InputType;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.orm.SugarTransactionHelper;
+import com.orm.query.Condition;
+import com.orm.query.Select;
+
+import neilw4.omin.R;
+import neilw4.omin.db.SecretKey;
+import neilw4.omin.db.UserId;
+import neilw4.omin.fetch_key.FetchKey;
+
+import static neilw4.omin.Logger.*;
+
+public class UnameManager {
+
+    final static String TAG = UnameManager.class.getSimpleName();
+
+    final Context mContext;
+
+    final EditText mEditUname;
+    final ImageButton mEditBtn;
+
+    final Drawable editTextBg;
+    private final InputMethodManager mInputManager;
+
+    boolean editable = false;
+    String uname = null;
+
+    public UnameManager(Context context, View root) {
+        mContext = context;
+        mEditUname = (EditText) root.findViewById(R.id.edit_uname_text);
+        mEditBtn = (ImageButton) root.findViewById(R.id.edit_uname);
+
+        mInputManager = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        editTextBg = mEditUname.getBackground();
+
+        SecretKey myKey = getKey();
+        if (myKey != null) {
+            uname = myKey.uid.uname;
+        }
+
+        mEditUname.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                makeEditable();
+            }
+        });
+
+        mEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editable) {
+                    endEdit();
+                } else {
+                    makeEditable();
+                }
+            }
+        });
+
+        configureView();
+    }
+
+    public void configureView() {
+        if (editable) {
+            mEditUname.setBackgroundDrawable(editTextBg);
+            mEditUname.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
+        } else {
+            mEditUname.setBackgroundResource(android.R.color.transparent);
+            mEditUname.setInputType(InputType.TYPE_NULL);
+        }
+
+        if (uname == null) {
+            if (editable) {
+                mEditUname.setText("");
+            } else {
+                mEditUname.setText(R.string.anonymous_uname);
+            }
+        } else {
+            mEditUname.setText(uname);
+        }
+    }
+
+    public void makeEditable() {
+        if (!editable) {
+            editable = true;
+            configureView();
+
+            // hack to force the keyboard to show
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    mEditUname.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_DOWN, 0, 0, 0));
+                    mEditUname.dispatchTouchEvent(MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), MotionEvent.ACTION_UP, 0, 0, 0));
+
+                }
+            }, 50);
+        }
+    }
+
+
+
+    public void endEdit() {
+        if (editable) {
+            editable = false;
+
+            uname = mEditUname.getText().toString();
+            if (uname.isEmpty()) {
+                uname = null;
+            }
+            mEditUname.clearFocus();
+            mInputManager.hideSoftInputFromWindow(mEditUname.getWindowToken(), 0);
+            saveUname();
+            configureView();
+        }
+    }
+
+    private SecretKey getKey() {
+        return Select.from(SecretKey.class).first();
+    }
+
+    public boolean saveUname() {
+        if (uname != null && !UserId.valid(uname)) {
+            uname = null;
+            return false;
+        }
+
+        new Thread() {
+            @Override
+            public void run() {
+                SugarTransactionHelper.doInTansaction(new SugarTransactionHelper.Callback() {
+                    @Override
+                    public void manipulateInTransaction() {
+                        SecretKey myKey = getKey();
+                        if (myKey == null || !myKey.uid.uname.equals(uname)) {
+                            if (myKey != null) {
+                                deleteKey(myKey);
+                            }
+                            if (uname != null) {
+                                UserId uid = makeUid();
+                                SecretKey key = new SecretKey(uid);
+                                key.save();
+                                info(TAG, "new uname");
+                                FetchKey.asyncFetch();
+                            }
+                        }
+                    }
+                });
+            }
+        }.start();
+        return true;
+    }
+
+    private UserId makeUid() {
+        UserId uid = Select.from(UserId.class).where(
+                Condition.prop("uname").eq(uname),
+                Condition.prop("parent").notLike("%")
+        ).first();
+
+        if (uid == null) {
+            uid = new UserId(uname, null);
+            debug(TAG, "Created new uid " + uname);
+            uid.save();
+        } else {
+            debug(TAG, "Found existing uid " + uname);
+        }
+        return uid;
+    }
+
+    private void deleteKey(SecretKey key) {
+        if (key != null) {
+            debug(TAG, "deleting key " + key.uid.uname);
+            for (UserId toDelete = key.uid; toDelete != null; toDelete = toDelete.parent) {
+                debug(TAG, "checking parent id " + toDelete.uname);
+                Select<UserId> children = Select.from(UserId.class).where(Condition.prop("parent").eq(toDelete.getId()));
+                if (children.count() == 0) {
+                    debug(TAG, "deleting parent id " + toDelete.uname);
+                    toDelete.delete();
+                }
+            }
+            key.delete();
+        }
+    }
+}
